d984deab144b10d2e26da8f744d5f31a5ad68229
trongkhoa <68lktrongkhoa@gmail.com>
2025-06-08 15:11:27 +0700

    COCOS-32/createCharactor | convert if else logic move to max min

--- assets/Script/Gameplay/Character/States/MovingState.js
+++ assets/Script/Gameplay/Character/States/MovingState.js
@@ -9,25 +9,29 @@ export default class MovingState extends State {
     }
 
     enter() {
-        let targetIndex = this.controller.currentPositionIndex;
-        
-        if (this.moveDirection === 'MOVE_UP' && this.controller.currentPositionIndex > 0) {
-            targetIndex--;
-        } else if (this.moveDirection === 'MOVE_DOWN' && this.controller.currentPositionIndex < this.controller.yPositions.length - 1) {
-            targetIndex++;
-        } else {
+        const currentIndex = this.controller.currentPositionIndex;
+        const maxIndex = this.controller.yPositions.length - 1;
+        let targetIndex = currentIndex;
+
+        if (this.moveDirection === 'MOVE_UP') {
+            targetIndex = currentIndex - 1;
+        } else if (this.moveDirection === 'MOVE_DOWN') {
+            targetIndex = currentIndex + 1;
+        }
+
+        targetIndex = Math.max(0, Math.min(targetIndex, maxIndex));
+
+        if (targetIndex === currentIndex) {
             this.controller.setState(new IdleState(this.controller));
             return;
         }
 
         this.controller.currentPositionIndex = targetIndex;
-        const targetY = this.controller.yPositions[this.controller.currentPositionIndex];
+        const targetY = this.controller.yPositions[targetIndex];
 
-        
         cc.tween(this.controller.node)
             .to(this.controller.moveDuration, { position: cc.v3(this.controller.node.x, targetY, 0) }, { easing: 'quadOut' })
             .call(() => {
-                
                 this.controller.setState(new IdleState(this.controller));
             })
             .start();
